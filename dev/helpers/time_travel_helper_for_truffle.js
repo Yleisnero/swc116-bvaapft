14361b0630bfc4f7d9ae5d921279fc0a5a3c3bd7
Yleisnero <fischi12325@gmail.com>
2023-10-20 13:24:50 +0200

    Enhance example

--- dev/helpers/time_travel_helper_for_truffle.js
+++ dev/helpers/time_travel_helper_for_truffle.js
@@ -1,8 +1,8 @@
 const Web3 = require("web3");
 // Truffle development provider
-// const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:9545'));
+const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:9545'));
 // Ganache provider
-const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545/'));
+// const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545/'));
 
 // const { time } = require('@openzeppelin/test-helpers');
 
